01ab5b259b9b23e87ec1a5537c7c7bcf48413b40
Diwanshu Shekhar <diwanshu@gmail.com>
2024-03-14 17:42:05 -0600

    added just score prompt

--- driver.py
+++ driver.py
@@ -13,7 +13,7 @@ from codeQual.gpt import ChatGPT
 
 SYSTEM = """You will be provided with a piece of Python code delimited by three backticks (```) followed by it's description delimited by three double quotes (\"\"\"). Your task is to follow the following steps to answer user queries.
 
-Step 1: Provide descriptive assessment of the quality of the code in terms of the following criteria - Functionality, Readability, Pythonic, Error Handling and Efficiency. The definitions of these criteria are given below:
+Step 1: Assess the quality of the code in terms of the following criteria - Functionality, Readability, Pythonic, Error Handling and Efficiency. The definitions of these criteria are given below:
 
 Functionality - does the code work as per the provided description?
 Readability - how cognitively heavy the code is for a human reading the code?
@@ -25,11 +25,9 @@ Step 2: From a score of 1 to 5 where 5 being of the highest quality, score each 
 
 ---
 
-Provide output for all above steps in a single JSON format as follows.
+Provide output for step 2 in a  JSON format as follows.
 ```json
-{"step1":{ "functionality": "...","readability": "...", "pythonic": "...", "error_handling": "...", "efficiency": "..."},
-"step2": {"functionality": "...","readability": "...", "pythonic": "...", "error_handling": "...", "efficiency": "..."},
-}
+{"step2": {"functionality": "...","readability": "...", "pythonic": "...", "error_handling": "...", "efficiency": "..."}}
 ```
 """
 
